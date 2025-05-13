5d6cd1d210225dc6301224e4ee211b42503c1f97
Flames <61213546+lucasser@users.noreply.github.com>
2025-05-13 11:34:06 -0700

    removed servo

--- cooling_no_wifi.ino
+++ cooling_no_wifi.ino
@@ -1,10 +1,3 @@
-#include <ESP32Servo.h>
-
-Servo servob1;
-Servo servob2;
-Servo servol1;
-Servo servol2;
-
 const int LIVING = 4;
 const int BED1 = 27;
 const int BED2 = 25;
@@ -12,10 +5,6 @@ const int SPEED1 = 19;
 const int SPEED2 = 21;
 const int SPEED3 = 22;
 const int ZoneValve = 23;
-const int SERVOB1 = 14;
-const int SERVOB2 = 12;
-const int SERVOL1 = 13;
-const int SERVOL2 = 15;
 
 
 
@@ -29,11 +18,6 @@ void initPins() {
   pinMode(SPEED2, OUTPUT);
   pinMode(SPEED3, OUTPUT);
   pinMode(ZoneValve, OUTPUT);
-
-  servob1.attach(SERVOB1);
-  servob2.attach(SERVOB2);
-  servol1.attach(SERVOL1);
-  servol2.attach(SERVOL2);
 }
 
 int getSpeed() {
@@ -41,28 +25,23 @@ int getSpeed() {
   switch (digitalRead(BED1)) {
     case HIGH:
       speed += 1;
-      servob1.write(100);
       break;
     case LOW:
-      servob1.write(0);
+      break;
   }
   switch (digitalRead(BED2)) {
     case HIGH:
       speed += 1;
-      servob2.write(100);
       break;
     case LOW:
-      servob2.write(0);
+      break;
   }
   switch (digitalRead(LIVING)) {
     case HIGH:
       speed += 2;
-      servol1.write(100);
-      servol2.write(100);
       break;
     case LOW:
-      servol1.write(0);
-      servol2.write(0);
+      break;
   }
   if (speed > 3) {
     speed = 3;
